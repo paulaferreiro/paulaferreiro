43f5a64c2c72186ad2a93a4178f54af79e4609be
Paula Ferreiro <pauferoya@iesxulianmagarinos.org>
2023-10-05 13:33:15 +0200

    boletines

--- Boletin 3/ejercicio4.py
+++ Boletin 3/ejercicio4.py
@@ -1,1 +1,3 @@
-entero=int()
+entero=int(input("Por favor introduzca un numero entero positivo: "))
+for i in range(0,0+entero):
+    print(i)
---
+++ Boletin 3/ejercicio5.py
@@ -0,0 +1,4 @@
+inversion=input("Por favor introduzca su cantiadad a invertir: ")
+interes=input("Por favor introduzca su interes anual: ")
+años=("Por favor introduzca los años: ")
+for i in range()
