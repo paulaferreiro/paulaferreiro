83ec27e49012847281ff7f1991e7c72ee36587f7
Paula Ferreiro <pauferoya@iesxulianmagarinos.org>
2023-10-23 11:29:13 +0200

    ficheros

--- Boletin 3/ejercicio5.py
+++ Boletin 3/ejercicio5.py
@@ -1,4 +1,7 @@
 inversion=input("Por favor introduzca su cantiadad a invertir: ")
 interes=input("Por favor introduzca su interes anual: ")
 años=("Por favor introduzca los años: ")
-for i in range()
+año=(1>0,1>0)
+calculo=int(interes*años)
+for i in range(calculo and año,(1>0,1<0)):
+    print("El capital total es" ,i,"en cada ",año)
---
+++ ficheros.py/1.py
@@ -0,0 +1,10 @@
+entero=int(input("Por favor introduzca un número entero entre el 1 y el 10:"))
+#abrimos fichero
+f=open("prueba.txt", "w")
+f.write("Esto es una prueba")
+
+#hasta 11 para que incluya el 10
+for i in range(1,11):
+    print(entero,"x",i,"=",entero*i)
+
+f.close()
