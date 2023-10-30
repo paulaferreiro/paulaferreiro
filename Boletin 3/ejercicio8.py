dbf27cd60fd450350f746339dbc79245b329949b
Paula Ferreiro <pauferoya@iesxulianmagarinos.org>
2023-10-30 11:21:42 +0100

    boletín 3

---
+++ Boletin 3/ejercicio8.py
@@ -0,0 +1,11 @@
+entero=int(input("Por favor introduzca un número entero: "))
+for i in range(1,1):
+    print(i)
+#for i in range(1+entero,entero):
+    print(i)
+#for i in range(2+entero,1+entero,entero):
+    print(i)
+#for i in range(4+entero,2+entero,1+entero,entero):
+    print(i)
+#for i in range(6+entero,4+entero,2+entero,1+entero):
+    print(i)
