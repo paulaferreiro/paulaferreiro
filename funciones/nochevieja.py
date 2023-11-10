4aa17ed1043eb1597d582d1db0bc14a2e0b4dd90
Paula Ferreiro <pauferoya@iesxulianmagarinos.org>
2023-11-10 11:57:35 +0100

    noche vieja

--- funciones/nochevieja.py
+++ funciones/nochevieja.py
@@ -1,2 +1,14 @@
 #las funciones empiezan por def y terminan en retun
-def f ()
+def falta (dia, mes):
+    anho=[31, 28 ,31 ,30 ,31 ,30 ,31 ,31 ,30 ,31 ,30 ,31]
+    res=anho[mes-1]-dia
+    temporal_anho=anho[mes:12]
+    for i in temporal_anho:
+        res=res+i
+    return(res)
+
+dia=int(input("Dia:"))
+
+
+   
+
